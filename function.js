8a73a7898ee80efe47c3033b1ec8a47dee1ed80d
Tino <tinomhedziso21@gmail.com>
2024-08-03 20:30:50 +0200

    Name change

---
+++ function.js
@@ -0,0 +1,21 @@
+/** @format */
+//Changed the script name from script to function, script was not working💪🏿
+// calculator program code
+
+const display = document.getElementById("display");
+
+function appendToDisplay(input) {
+  display.value += input;
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = "Error";
+  }
+}
